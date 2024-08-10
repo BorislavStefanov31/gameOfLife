7df32a47188a17cc16abf5f8983de827e0f42e8c
Borislav Stefanov <125276636+BorislavStefanov31@users.noreply.github.com>
2024-08-10 15:33:36 +0300

    improvements

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from './src/screens';
 
 const Stack = createStackNavigator();
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Game of Life">
         <Stack.Screen name="Game of Life" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
